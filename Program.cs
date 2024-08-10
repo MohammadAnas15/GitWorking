5e774109bb674141b77ff2ed46ca8d0c65108e4c
MOHAMMAD ANAS <92999181+MohammadAnas15@users.noreply.github.com>
2024-08-10 09:06:21 +0500

    SomeChangesIN PrrogramFile

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string input="";
+while (input!="stop")
+{
+    Console.WriteLine("Enter some thing");
+     input = Console.ReadLine();
+}
+
+Console.WriteLine("The End.....!");
